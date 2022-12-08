d1a73fac9e8607d8e6fa78dc75d6d9b9571c246a
Callum <35613@ma-web.nl>
2022-12-08 12:47:22 +0100

    p

--- button/main.js
+++ button/main.js
@@ -1,5 +1,30 @@
-const canvas = document.getElementById("button")
+class App{
+    runApplication(){
+        let myCanvas = document.getElementById("canvasID")
+        let g = myCanvas.getContext("2d");
 
-window.onmousemove = function(event){
-    console.log("je hebt geklikt")
+        myCanvas.addEventListener('mousemove', (evt)=>{
+            let mousePos = this.getMousePos(myCanvas, evt);
+            console.log(mousePos.x)
+
+            g.clearRect(0, 0, myCanvas.width, myCanvas.height);
+            g.beginPath();
+            g.fillStyle = "purple";
+            g.rect(mousePos.x, mousePos.y, 30, 30);
+            g.stroke();
+            g.fill();
+        })
+
+    }
+
+    getMousePos(canvas, evt) {
+        let rect = canvas.getBoundingClientRect();
+        return {
+            x: evt.clientX - rect.left,
+            y: evt.clientY - rect.top
+        };
+    }
 }
+
+let app = new App();
+app.runApplication();
